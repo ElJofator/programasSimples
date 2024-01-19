f2a967547a204c4382560a4b0015b29c4dede8bc
ElJofator <79776204+ElJofator@users.noreply.github.com>
2024-01-19 13:00:43 -0500

    Retirado buenos días del saludo

--- saludo.py
+++ saludo.py
@@ -1,2 +1,2 @@
 #Escriba un programa que pida al usuario que escriba su nombre, y lo salude llamándolo por su nombre.
-print(f"Hola buenos días, {input('Ingrese su nombre: ')}")
+print(f"Hola, {input('Ingrese su nombre: ')}")
